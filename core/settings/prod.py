1e13b55dd38d95626861dd1d8610971c13006173
Givs01 <gaurav.kushwaha@CEPT.IN>
2025-11-27 17:27:17 +0530

    Initial commit with css

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -3,10 +3,14 @@ from .base import *  # noqa
 
 DEBUG = False
 
+ALLOWED_HOSTS = ['c-regist.onrender.com']
+
+
 # These must be provided by hosting
 # Example: ALLOWED_HOSTS=mydomain.com
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+
 
 # Security headers
 SECURE_BROWSER_XSS_FILTER = True
